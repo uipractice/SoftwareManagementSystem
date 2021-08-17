9c08f5c87e839c24821edc7f8101a6ba4ba11c19
Sai Rajesh Paleti <spaleti@evoketechnologies.com>
2021-08-17 17:15:44 +0530

    SMS-Provide-FeedBack-Mail: provide feedback mail functionality

--- src/app/components/NavBar/Footer.js
+++ src/app/components/NavBar/Footer.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import Feedback from '../admin/Feedback';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+// helpers
+import { getApiUrl } from '../utils/helper';
 
 export default function Footer() {
   const [open, setOpen] = React.useState(false);
+  const history = useHistory();
+  /**
+   * Setting modal close state and call api to send the mail.
+   *
+   * @param {Object} event current event object.
+   * @param {Boolean} closeClick contains boolean to defined the close click.
+   * @return {null}
+   */
+  const handleClose = (event, closeClick) => {
+    setOpen(false);
+    if (!closeClick) {
+      axios
+        .post(getApiUrl(`softwareInfo/feedbackMail`))
+        .then((res) => {
+          console.log(res.data);
+          toast.success('A Reminder mail has been triggered !', {
+            autoClose: 1800,
+          });
+        })
+        .catch((err) => console.log(err.response));
+
+      setTimeout(() => {
+        history.push('/admin');
+      }, 2000);
+    }
+  };
 
   return (
     <div className='footer'>
@@ -15,7 +46,10 @@ export default function Footer() {
             Provide Feedback
           </button>
         </li>
-        <Feedback isOpen={open} closeModal={() => setOpen(false)} />
+        <Feedback
+          isOpen={open}
+          closeModal={(e, closeClick) => handleClose(e, closeClick)}
+        />
       </ul>
     </div>
   );
