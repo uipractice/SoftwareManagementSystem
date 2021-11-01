ee8633eb1e3f3d6900ea0124f489ba313ff07f99
Sai Rajesh Paleti <spaleti@evoketechnologies.com>
2021-11-01 22:36:49 +0530

    Code updated

--- src/app/components/login/Login.js
+++ src/app/components/login/Login.js
@@ -33,6 +33,11 @@ function Login() {
  
   function handleLogin(e) {
     e.preventDefault();
+    setUser({
+      ...user,
+      userName: "",
+      password: "",
+    });
     AuthServices.login(user)
       .then((res) => {
         if (res.data.accessToken) {
@@ -42,13 +47,6 @@ function Login() {
         } else {
           toast.error(res.data.message + ` ${"!!"}`, {
             autoClose: 2000,
-            onClose: () => {
-              setUser({
-                ...user,
-                userName: "",
-                password: "",
-              });
-            },
           });
         }
       })
